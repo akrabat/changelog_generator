314ac009397a29cc11ba03d782ce7b79cc1a0d37
Rob Allen <rob@akrabat.com>
2021-08-12 15:01:31 +0100

    Tidy up error messages
    We don't need to display the exception message when we can't find a
    milestone. Also add PHP_EOL where required.

--- changelog_generator.php
+++ changelog_generator.php
@@ -170,12 +170,12 @@ function getConfig()
         ));
         $opts->parse();
     } catch (Exception $e) {
-        file_put_contents('php://stderr', $e->getMessage());
+        file_put_contents('php://stderr', $e->getMessage() . PHP_EOL);
         exit(1);
     }
 
     if (isset($opts->h) || $opts->toArray() == array()) {
-        file_put_contents('php://stdout', $opts->getUsageMessage());
+        file_put_contents('php://stdout', $opts->getUsageMessage() . PHP_EOL);
         exit(0);
     }
 
@@ -284,9 +284,8 @@ function getMilestonePayload($client, $user, $repo, $milestone)
         fwrite(
             STDERR,
             sprintf(
-                'Provided milestone ID [%s] does not exist: %s%s',
+                'Provided milestone ID [%s] does not exist.%s',
                 $milestone,
-                $milestoneResponseBody ?: 'Unknown error',
                 PHP_EOL
             )
         );
@@ -330,9 +329,8 @@ function getMilestoneByTitle($client, $user, $repo, $milestoneTitle)
     } while (true);
 
     fwrite(STDERR, sprintf(
-        'Provided milestone title [%s] does not exist: %s%s',
+        'Provided milestone title [%s] does not exist.%s',
         $milestoneTitle,
-        $milestoneResponseBody,
         PHP_EOL
     ));
 
